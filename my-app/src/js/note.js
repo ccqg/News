99ad32eaba6458dc5a66667b4df86188e3ebecc8
ccqg <gabaleschrist1@gmail.com>
2024-05-08 23:15:21 +0800

    New
    Co-Authored-By: The Strongest Algorithm <159101935+centmarde@users.noreply.github.com>

--- my-app/src/js/note.js
+++ my-app/src/js/note.js
@@ -53,7 +53,7 @@ async function getDatas(keyword = "") {
 
   let container = "";
   notes.forEach((datas) => {
-    container += `<div class=" card w-100 mb-3 shadow">
+    container += `<div class="mt-2"><div class=" card w-100">
     <div id="card_color" class="card-body rounded"  >
       <h5 class="card-title">${datas.title}</h5>
       <p class="card-text">${datas.description}</p>
@@ -87,7 +87,7 @@ async function getDatas(keyword = "") {
     </div>
   </div>
 </div>
-  
+</div>
   `;
   });
 
